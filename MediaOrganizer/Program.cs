7a0bd5e4faf2bb0b7d7b574dca3b2183e790f1f1
estrellasoft <mark@estrellasoft.com>
2017-07-11 21:45:43 -0700

    Add files via upload

---
+++ MediaOrganizer/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+using System.Web;
+
+
+namespace MediaOrganizer
+{
+    class Program
+    {
+        static string movieFolder = ConfigurationManager.AppSettings["moviesFolder"];
+        static string logfolder = ConfigurationManager.AppSettings["logfolder"];
+        static string logfilename = ConfigurationManager.AppSettings["logfilename"];
+ 
+        private static List<string> excludedDirectories = new List<string>() {"genre","Genre"};
+        static void Main(string[] args)
+        {
+
+            //Get a list of all the movie folders excluse the genres folder which is also off the root
+            var filteredDirs = Directory.GetDirectories(movieFolder).Where(d => !isExcluded(excludedDirectories, d));
+
+            //rename the movies in the folder to the folder name
+              foreach (string folderName in filteredDirs)
+            {
+                string movieName = new DirectoryInfo(folderName).Name;
+                DirectoryInfo d = new DirectoryInfo(folderName);//
+                FileInfo[] files = d.GetFiles("*.mp4"); //Getting Media files
+
+                //move movies from their folder to the root
+                foreach (FileInfo file in files)
+                {
+                    System.IO.File.Move(folderName + "\\" + file.Name, movieFolder + "\\" + movieName + ".mp4");
+                    Console.WriteLine("Renaming " + folderName + "\\" + file.Name + " to " + movieFolder + "\\" + movieName + ".mp4");
+                   
+                    //Log anything that affects the fiole system with a timestamp
+                    using (StreamWriter w = File.AppendText(logfolder + logfilename))
+                    {
+                        Log("Renaming " + folderName + "\\" + file.Name + " to " + movieFolder + "\\" + movieName + ".mp4", w);
+                    }
+                }
+
+                // if directory is empty 
+                if (IsDirectoryEmpty(d))
+                {
+                    d.Delete();
+                    Console.WriteLine("Folder " + d + " deleted");
+                }
+
+                else
+                {
+                    Console.WriteLine("Folder " + d + " not empty. See log for details.");
+                    using (StreamWriter w = File.AppendText(logfolder + logfilename))
+                    {
+                        Log("Folder " + d + " not empty", w);
+                    }
+                }
+              
+            }
+
+              classifyTitles();
+              Console.WriteLine("Process Completed.Press any key to close.");
+            Console.ReadLine();
+        }
+
+        public static void classifyTitles()
+        {
+            //get genre for all movies in root and move to existing folder or create new folder.
+            
+            DirectoryInfo d = new DirectoryInfo(movieFolder);//
+                FileInfo[] files = d.GetFiles("*.mp4"); //Getting Media files
+
+                //move movies from root to genre
+                foreach (FileInfo file in files)
+                {
+                    //replace underscores with plus "+"
+                    string baseFilename = file.Name.Replace(file.Extension, "");
+                    baseFilename = baseFilename.Replace("_", "+");
+                    string genre = getMovieDetails(baseFilename);
+
+                    // if folder exists copy to folder
+                    //remember to check CASE
+
+                    try
+                    {
+                        //create new folder then copy
+                        if (!Directory.Exists(movieFolder + "\\" + "Genre" + "\\" + genre))
+                        {
+                            // Try to create the directory.
+                            DirectoryInfo di = Directory.CreateDirectory(movieFolder + "\\" + "Genre" + "\\" + genre);
+                           
+                        }
+                        
+                            
+                        else
+                        {
+
+                        }
+                        //copy to existing/new  folder
+                        System.IO.File.Move(movieFolder + "\\" + file, movieFolder + "\\" + "Genre" + "\\" + genre + "\\" + file);
+                    }
+                    catch (IOException ioex)
+                    {
+                        Console.WriteLine(ioex.Message);
+                    }
+                    //if folder does not exist, created foler, then copy file
+                }
+        }
+
+        public static string getMovieDetails(string movieTitle)
+        {
+            //handle possibility that multiple titles can be returned
+            //string movieTitle = "resurrection+of+gavin+stone";
+            string genre = WebApi.callImdb(movieTitle);
+            //Console.WriteLine(genre);
+            return genre;
+            
+        }
+
+        static bool isExcluded(List<string> exludedDirList, string target)
+        {
+            return exludedDirList.Any(d => new DirectoryInfo(target).Name.Equals(d));
+        }
+
+        public static bool IsDirectoryEmpty(DirectoryInfo directory)
+        {
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] subdirs = directory.GetDirectories();
+
+            return (files.Length == 0 && subdirs.Length == 0);
+        }
+
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine("{0} {1}", DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString());
+            w.WriteLine("{0}", logMessage);
+
+        }
+      
+    }
+}
---
+++ MediaOrganizer/WebApi.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+using System.Web;
+using System.Net;
+using System.Web.Script;
+using System.Web.Script.Serialization;
+
+namespace MediaOrganizer
+{
+    class WebApi
+    {
+        public static string callImdb(string txtMovieName)
+        {
+            {
+                string apikey = ConfigurationManager.AppSettings["apikey"];
+                string apiurl = ConfigurationManager.AppSettings["apiurl"];
+                string url = apiurl + txtMovieName.Trim() + "&" + apikey;
+                
+                using (WebClient wc = new WebClient())
+                {
+                    try
+                    {
+                        string html = wc.DownloadString(url);
+                        var json = wc.DownloadString(url);
+                        JavaScriptSerializer oJS = new JavaScriptSerializer();
+                        ImdbEntity obj = new ImdbEntity();
+                        obj = oJS.Deserialize<ImdbEntity>(json);
+                        if (obj.Response == "True")
+                        {
+                            string data = obj.Genre.ToString();
+                            string date = obj.Released.ToString();
+
+                            DateTime releaseDate = Convert.ToDateTime(date);
+                            TimeSpan ts = DateTime.Now - releaseDate;
+                            int differenceInDays = ts.Days;
+                            if (differenceInDays < 180)
+                            {
+                                return "New Releases";
+                            }
+                            else
+                            {
+                                string value = data.Split(',')[0];
+                                return value;
+                            }
+                           
+                        }
+                        else
+                        {
+                            return "unknown";
+                        }
+
+                    }
+
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message.ToString() + ", " + ex.InnerException.ToString());
+                        return "Error, chek log";
+                    }
+                }
+            }
+        }
+
+        public class ImdbEntity
+        {
+            public string Title { get; set; }
+            public string Year { get; set; }
+            public string Rated { get; set; }
+            public string Released { get; set; }
+            public string Runtime { get; set; }
+            public string Genre { get; set; }
+            public string Director { get; set; }
+            public string Writer { get; set; }
+            public string Actors { get; set; }
+            public string Plot { get; set; }
+            public string Language { get; set; }
+            public string Country { get; set; }
+            public string Awards { get; set; }
+            public string Poster { get; set; }
+            public string Metascore { get; set; }
+            public string imdbRating { get; set; }
+            public string imdbVotes { get; set; }
+            public string imdbID { get; set; }
+            public string Type { get; set; }
+            public string Response { get; set; }
+        }
+    }
+}
